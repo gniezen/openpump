6d8c39853e30ecd2c8b760120eedb74d97387837
Gerrit Niezen <gerrit.niezen@gmail.com>
2014-08-22 15:55:14 +0100

    Added UI Pack assets and added a few buttons

---
+++ ui/README.md
@@ -0,0 +1,8 @@
+
+## Setup
+Prerequisites:
+- Install PIL using `sudo pip install pil` 
+- Install Gameduino 2 asset converter and Python libraries using `sudo pip install gameduino2`
+
+Note: On Mac you can also install `sudo pip install pillow` and port Gameduino 2 to use Pillow instead of PIL: http://pillow.readthedocs.org/en/latest/porting-pil-to-pillow.html
+
--- ui/ui.ino
+++ ui/ui.ino
@@ -2,9 +2,11 @@
 #include <SPI.h>
 #include <GD2.h>
 
-#include "spinner_assets.h"
+//#include "spinner_assets.h"
+#include "default_assets.h"
 
 int i = 0;
+  int pressed = 0;
 
 void setup()
 {
@@ -14,10 +16,15 @@ void setup()
 
 void loop()
 {
+  
+
+  
   //GD.ClearColorRGB(0x103000);
+  GD.get_inputs();
   GD.Clear();
   GD.cmd_text(240, 68, 31, OPT_CENTER, "56.7 mL/h");
   
+  /*
   GD.Begin(POINTS); // draw 50-pixel wide green circles
   GD.ColorRGB(20, 91, 71);
   GD.PointSize(50 * 16);
@@ -25,12 +32,26 @@ void loop()
   // additive blending
   for (int x = 100; x <= 380; x += 40)
   GD.Vertex2ii(x, 200);
+  */
   
   GD.Begin(BITMAPS);
+  /*
   GD.Vertex2ii(240, 110, SPINNER_HANDLE, i);
   i += 1;
   if(i>11)
     i = 0;
-    
+  */
+
+  GD.Vertex2ii(240,110,GREY_PANEL_HANDLE);
+  
+  GD.Tag(1);
+  if(pressed == 1) {
+    GD.Vertex2ii(250,120,GREEN_BUTTON03_HANDLE);
+  }else{
+    GD.Vertex2ii(250,120,GREEN_BUTTON02_HANDLE);
+  }
+ 
+  pressed = GD.inputs.tag;
+  
   GD.swap();
 }
